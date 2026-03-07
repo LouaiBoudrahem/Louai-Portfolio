f3c58b6c290751bb1925bf1d350d7fa8a1d5a54f
Louaios <l_boudrahem@estin.dz>
2026-03-07 18:16:26 +0100

    Home page navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 import { logo } from "../assets/images";
 
 const Navbar = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <header className='header'>
       <NavLink to='/'>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
+        {!isHomePage && (
+          <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+            Home
+          </NavLink>
+        )}
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
           About
         </NavLink>
